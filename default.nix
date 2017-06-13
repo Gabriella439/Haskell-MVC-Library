65e532dacbd5775e3a0029e6a4e70097a3254d73
Gabriel Gonzalez <gabriel@awakenetworks.com>
2017-06-12 18:33:09 -0700

    Add Nix derivations to build the package

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, async, base, contravariant, foldl, managed, mmorph
+, pipes, pipes-concurrency, stdenv, transformers
+}:
+mkDerivation {
+  pname = "mvc";
+  version = "1.1.3";
+  src = ./.;
+  libraryHaskellDepends = [
+    async base contravariant foldl managed mmorph pipes
+    pipes-concurrency transformers
+  ];
+  description = "Model-view-controller";
+  license = stdenv.lib.licenses.bsd3;
+}
